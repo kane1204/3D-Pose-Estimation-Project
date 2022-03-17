dc2648ca62ca4f42cf2d5265851c8e07411282ad
JK <jonathankane1204@yahoo.co.uk>
2022-03-17 19:59:06 +0000

    KEP-45 Working on new acc metric PCK & 3D out

--- notebooks/src/eval/accuracies.py
+++ notebooks/src/eval/accuracies.py
@@ -99,3 +99,114 @@ def get_max_preds(batch_heatmaps):
 
     preds *= pred_mask
     return preds, maxvals
+
+
+def keypoint_3d_pck(pred, gt, mask, alignment='none', threshold=0.15):
+    """Calculate the Percentage of Correct Keypoints (3DPCK) w. or w/o rigid
+    alignment.
+    Paper ref: `Monocular 3D Human Pose Estimation In The Wild Using Improved
+    CNN Supervision' 3DV`2017
+    More details can be found in the `paper
+    <https://arxiv.org/pdf/1611.09813>`__.
+    batch_size: N
+    num_keypoints: K
+    keypoint_dims: C
+    Args:
+        pred (np.ndarray[N, K, C]): Predicted keypoint location.
+        gt (np.ndarray[N, K, C]): Groundtruth keypoint location.
+        mask (np.ndarray[N, K]): Visibility of the target. False for invisible
+            joints, and True for visible. Invisible joints will be ignored for
+            accuracy calculation.
+        alignment (str, optional): method to align the prediction with the
+            groundtruth. Supported options are:
+            - ``'none'``: no alignment will be applied
+            - ``'scale'``: align in the least-square sense in scale
+            - ``'procrustes'``: align in the least-square sense in scale,
+                rotation and translation.
+        threshold:  If L2 distance between the prediction and the groundtruth
+            is less then threshold, the predicted result is considered as
+            correct. Default: 0.15 (m).
+    Returns:
+        pck: percentage of correct keypoints.
+    """
+    assert mask.any()
+
+    if alignment == 'none':
+        pass
+    elif alignment == 'procrustes':
+        pred = np.stack([
+            compute_similarity_transform(pred_i, gt_i)
+            for pred_i, gt_i in zip(pred, gt)
+        ])
+    elif alignment == 'scale':
+        pred_dot_pred = np.einsum('nkc,nkc->n', pred, pred)
+        pred_dot_gt = np.einsum('nkc,nkc->n', pred, gt)
+        scale_factor = pred_dot_gt / pred_dot_pred
+        pred = pred * scale_factor[:, None, None]
+    else:
+        raise ValueError(f'Invalid value for alignment: {alignment}')
+
+    error = np.linalg.norm(pred - gt, ord=2, axis=-1)
+    pck = (error < threshold).astype(np.float32)[mask].mean() * 100
+
+    return pck
+
+# ------------------------------------------------------------------------------
+# Adapted from https://github.com/akanazawa/hmr
+# Original licence: Copyright (c) 2018 akanazawa, under the MIT License.
+# ------------------------------------------------------------------------------
+def compute_similarity_transform(source_points, target_points):
+    """Computes a similarity transform (sR, t) that takes a set of 3D points
+    source_points (N x 3) closest to a set of 3D points target_points, where R
+    is an 3x3 rotation matrix, t 3x1 translation, s scale. And return the
+    transformed 3D points source_points_hat (N x 3). i.e. solves the orthogonal
+    Procrutes problem.
+    Notes:
+        Points number: N
+    Args:
+        source_points (np.ndarray([N, 3])): Source point set.
+        target_points (np.ndarray([N, 3])): Target point set.
+    Returns:
+        source_points_hat (np.ndarray([N, 3])): Transformed source point set.
+    """
+
+    assert target_points.shape[0] == source_points.shape[0]
+    assert target_points.shape[1] == 3 and source_points.shape[1] == 3
+
+    source_points = source_points.T
+    target_points = target_points.T
+
+    # 1. Remove mean.
+    mu1 = source_points.mean(axis=1, keepdims=True)
+    mu2 = target_points.mean(axis=1, keepdims=True)
+    X1 = source_points - mu1
+    X2 = target_points - mu2
+
+    # 2. Compute variance of X1 used for scale.
+    var1 = np.sum(X1**2)
+
+    # 3. The outer product of X1 and X2.
+    K = X1.dot(X2.T)
+
+    # 4. Solution that Maximizes trace(R'K) is R=U*V', where U, V are
+    # singular vectors of K.
+    U, _, Vh = np.linalg.svd(K)
+    V = Vh.T
+    # Construct Z that fixes the orientation of R to get det(R)=1.
+    Z = np.eye(U.shape[0])
+    Z[-1, -1] *= np.sign(np.linalg.det(U.dot(V.T)))
+    # Construct R.
+    R = V.dot(Z.dot(U.T))
+
+    # 5. Recover scale.
+    scale = np.trace(R.dot(K)) / var1
+
+    # 6. Recover translation.
+    t = mu2 - scale * (R.dot(mu1))
+
+    # 7. Transform the source points:
+    source_points_hat = scale * R.dot(source_points) + t
+
+    source_points_hat = source_points_hat.T
+
+    return source_points_hat
--- notebooks/src/train_lifting_network.py
+++ notebooks/src/train_lifting_network.py
@@ -3,8 +3,11 @@ import torch
 from tqdm.notebook import tqdm
 import torch.nn as nn
 import gc
+import time
+
+from notebooks.src.eval.accuracies import keypoint_3d_pck
 class Train_LiftNetwork():
-    def __init__(self,model,optimiser,accurate_dist,train_dataloader,valid_dataloader,reducedkey=False):
+    def __init__(self,model,optimiser,accurate_dist,train_dataloader,valid_dataloader):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         print("Using {} device".format(self.device))   
         self.model = model.to(self.device)
@@ -13,83 +16,97 @@ class Train_LiftNetwork():
         self.accz_dists = accurate_dist
         self.train_ds = train_dataloader
         self.valid_ds = valid_dataloader
-        self.reduce = reducedkey
-        self.reducedKeypoints = [0,2,3,4,6 , 7,10,13 , 14,17,20, 21,24,27, 28,31,34, 35,38,41, 42,45,48, 52,53,55, 58,59,61]
-
+        
     def train_step(self):
-        size = len(self.train_ds.dataset)
-        correct = 0
-        losses = 0
-
+        train_epoch_acc, train_epoch_loss = 0, 0
+        batches = 0
         self.model.train()
-        for data in tqdm(self.train_ds, desc="Training Step"):
+        for data in tqdm(self.train_ds, desc="Training Step", disable=True):
             X = data['key_points_2D']
-            y = data['key_points_3D'][:,:,2]
+            y = data['key_points_3D'] # 3d
             mask = data['visibility'].to(self.device)
             
             X, y = X.to(self.device, dtype=torch.float), y.to(self.device, dtype=torch.float)
-            
-            X_var = torch.autograd.Variable(X)
-            y_var = torch.autograd.Variable(y)
-            # y_var = torch.flatten(y_var, start_dim=1, end_dim=2)
+            # 3dim to unflatten reshape((batch,limbs,3))
+            X_flattened = torch.flatten(X, start_dim=1, end_dim=2)
+            y_flattened  = torch.flatten(y, start_dim=1, end_dim=2)
+            mask_flattened = torch.flatten(torch.stack([mask,mask,mask],dim=1),start_dim=1, end_dim= 2)
 
-            pred = self.model(torch.flatten(X_var, start_dim=1, end_dim=2))
+            pred = self.model(X_flattened)
 
-            # For 3 Dims 
-            # mask = torch.flatten(torch.stack([mask,mask,mask],dim=1),start_dim=1, end_dim= 2)
-
-            loss = torch.mean(((pred - y_var)*mask)**2)
-            # correct += (abs(pred - y_var)<torch.flatten(self.accz_dists.to(self.device).T)).type(torch.float).sum().item()
+            train_loss = torch.mean(((pred - y_flattened)*mask_flattened)**2)
+            train_acc = keypoint_3d_pck(pred.detach().cpu().numpy().reshape((len(pred),28,3)),
+                                        y.detach().cpu().numpy(), mask)
 
             # Backpropagation
             self.optimiser.zero_grad()
-            loss.backward()
+            train_loss.backward()
             self.optimiser.step()
             
-            losses += loss.item()
+            train_epoch_loss += train_loss.item()
+            train_epoch_acc += train_acc.item()
+            batches += 1
+            # Stops training after one batch
+            # break
 
-        train_acc = (correct / size)*100
-        train_loss = losses/size
-        return train_acc, train_loss
+        # Normal calculation
+            train_acc = train_epoch_acc/batches
+            train_loss = train_epoch_loss/batches
+            return train_acc, train_loss
 
     def valid_step(self):
-        size = len(self.valid_ds.dataset)
-        num_batches = len(self.valid_ds)
+        val_epoch_acc, val_epoch_loss = 0, 0
+        batches = 0
         self.model.eval()
-        test_loss, correct = 0, 0
         with torch.no_grad():
-            for data in tqdm(self.valid_ds, desc="Validation Step"):
+            for data in tqdm(self.valid_ds, desc="Validation Step", disable=True):
                 X = data['key_points_2D']
-                y = data['key_points_3D'][:,:,2]
+                y = data['key_points_3D'] # 3d
                 mask = data['visibility'].to(self.device)
 
                 X, y = X.to(self.device, dtype=torch.float), y.to(self.device, dtype=torch.float)
-                
-                X_var = torch.autograd.Variable(X)
-                y_var = torch.autograd.Variable(y)
-                # y_var = torch.flatten(y_var, start_dim=1, end_dim=2)
+                # 3dim to unflatten reshape((batch,limbs,3))
+                X_flattened = torch.flatten(X, start_dim=1, end_dim=2)
+                y_flattened  = torch.flatten(y, start_dim=1, end_dim=2)
+                mask_flattened = torch.flatten(torch.stack([mask,mask,mask],dim=1),start_dim=1, end_dim= 2)
 
-                pred = self.model(torch.flatten(X_var, start_dim=1, end_dim=2))
-                
-                # For 3 Dims 
-                # mask = torch.flatten(torch.stack([mask,mask,mask],dim=1),start_dim=1, end_dim= 2)
+                pred = self.model(X_flattened)
 
-                loss = torch.mean(((pred - y_var)*mask)**2)
-                test_loss += loss.item()
-                # correct += (abs(pred - y_var)<torch.flatten(self.accz_dists.to(self.device).T)).type(torch.float).sum().item()
-        test_loss /= num_batches
+                val_loss = torch.mean(((pred - y_flattened)*mask_flattened)**2)
+                val_acc = keypoint_3d_pck(pred.detach().cpu().numpy().reshape((len(pred),28,3)),
+                                        y.detach().cpu().numpy(), mask)
+                val_epoch_loss += val_loss.item()
+                val_epoch_acc += val_acc.item()
+                batches +=1
 
-        val_acc = (correct / size)*100
-        val_loss = test_loss
+
+        val_acc = val_epoch_acc/batches
+        val_loss = val_epoch_loss/batches
         # print(f"Validation Error: \n Accuracy: {val_acc:>4f}%, Avg loss: {val_loss:>8f} \n")
-        return val_acc, val_loss
+        return val_epoch_acc, val_epoch_loss
         
     def run(self, epochs):
-        for t in tqdm(range(1, epochs+1), desc="CPM Model Epochs"):
-            print(f'Epoch {t+0:03}:')
+        torch.cuda.empty_cache()
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        file_desc = f"lifting_3d_model_{timestr}"
+        for t in tqdm(range(1, epochs+1), desc="Lifting Model Epochs"):
+            train_acc, train_loss, val_acc, val_loss  = 0,0,0,0
+
             train_acc, train_loss = self.train_step()
             val_acc, val_loss = self.valid_step()
-            print(f'Finished Epoch {t+0:03}: | Train Loss: {train_loss:.5f} | Val Loss: {val_loss:.5f} | Train Acc: {train_acc:.3f}| Val Acc: {val_acc:.3f}')
+
+            # Append Training and validation stats to file
+            self.append_file(f"{file_desc}_train_loss", train_loss)
+            self.append_file(f"{file_desc}_train_acc", train_acc)
+            self.append_file(f"{file_desc}_val_loss", val_loss)
+            self.append_file(f"{file_desc}_val_acc", val_acc)
+            print(f'Finished Epoch {t+0:03}: | Train Acc: {train_acc:.3f} | Train Loss: {train_loss:.5f} | Val Acc: {val_acc:.3f} | Val Loss: {val_loss:.5f}')
+
         print("Done!")
         return self.model
+
+    def append_file(self,filename, data):
+        file1 = open(f"../results/{filename}.txt", "a")  # append mode
+        file1.write(f"{data}\n")
+        file1.close()
 
